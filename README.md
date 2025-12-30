88ced53c2f4d8de3c617a548c3147e85d98e80f1
David Snopek <dsnopek@gmail.com>
2025-12-30 08:08:44 -0600

    Copy rootfs-related files from the Android app

---
+++ README.md
@@ -0,0 +1,7 @@
+Rootfs for Godot Android Build Environment (or GABE)
+====================================================
+
+This repo contains the Linux rootfs used by the Godot Android Build Environment (or GABE).
+
+See https://github.com/godotengine/android-editor-buildenv-app
+
---
+++ docker/build-rootfs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PLATFORM="linux/arm64/v8"
+#PLATFORM="linux/amd64"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ALPINE_ANDROID_DIR="$SCRIPT_DIR/../thirdparty/alpine-android"
+BUILD_DIR="$SCRIPT_DIR/build"
+
+IMAGE_NAME=godotengine/alpine-android:android-35-jdk17
+
+# Build the alpine-android images.
+
+docker build "$ALPINE_ANDROID_DIR/docker" --platform=$PLATFORM -f "$ALPINE_ANROID_DIR/docker/base.Dockerfile" -t alvrme/alpine-android-base:jdk17 \
+	--build-arg="JDK_VERSION=17" --build-arg="CMDLINE_VERSION=latest" --build-arg="SDK_TOOLS_VERSION=13114758"
+
+docker build "$ALPINE_ANDROID_DIR/docker" --platform=$PLATFORM -f "$ALPINE_ANDROID_DIR/docker/android.Dockerfile" -t alvrme/alpine-android:android-35-jdk17 \
+	--build-arg="JDK_VERSION=17" --build-arg="BUILD_TOOLS=35.0.0" --build-arg="TARGET_SDK=35"
+
+docker build "$SCRIPT_DIR" --platform=$PLATFORM -f "$SCRIPT_DIR/Dockerfile.godot" -t $IMAGE_NAME \
+	--build-arg="JDK_VERSION=17" --build-arg="BUILD_TOOLS=35.0.0" --build-arg="TARGET_SDK=35"
+
+mkdir -p "$BUILD_DIR"
+
+CONTAINER_ID=$(docker create --platform=$PLATFORM $IMAGE_NAME)
+docker export --output="$BUILD_DIR/alpine-android-35-jdk17.tar" $CONTAINER_ID
+#docker inspect $IMAGE_NAME --format='{{json .Config.Env}}' | python -c 'import json, sys; d=json.load(sys.stdin); print(*d, sep="\n")' > "$BUILD_DIR/alpine-android-35-jdk17.env"
+docker rm $CONTAINER_ID
+
+rm -f "$BUILD_DIR/alpine-android-35-jdk17.tar.xz"
+(cd "$BUILD_DIR" && xz -T0 alpine-android-35-jdk17.tar)
